4eb5d34b874363dbd123b1520ba48751f60bcd81
MAparicioTorres <ma.aparicio.torres@gmail.com>
2026-02-16 23:34:57 +0100

    Setup models, env and tests

---
+++ app/Models/Idea.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\IdeaStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Idea extends Model
+{
+    /** @use HasFactory<\Database\Factories\IdeaFactory> */
+    use HasFactory;
+
+    protected function casts()
+    {
+        return [
+            'links' => 'array',
+            'status' => IdeaStatus::class
+        ];
+    }
+
+    protected $attributes = [
+        'links' => '[]',
+        'status' => IdeaStatus::PENDING->value,
+
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(related: Step::class);
+    }
+}
---
+++ database/factories/IdeaFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Idea>
+ */
+class IdeaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            // JSON column → array
+            'links' => [
+                fake()->url
+            ]
+        ];
+    }
+}
